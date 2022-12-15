7cdbdaeff7f7a676bb6d8505248f13ad37372d06
Ezgi Pektaş <ezgiipektas@gmail.com>
2022-12-15 04:04:24 +0300

    2.0

---
+++ RealEstate.PresentationLayer/Areas/Default/Controllers/AboutProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate.BusinessLayer.Abstract;
+
+namespace RealEstate.PresentationLayer.Areas.Default.Controllers
+{
+    [AllowAnonymous]
+    [Area("Default")]
+    public class AreasProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public AreasProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
